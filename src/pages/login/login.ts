8fc448fbd7d7a580270b0a3539ba2c639ae18dc6
majlo23 <majo.simovic@gmail.com>
2017-04-03 08:10:04 +0200

    serus ide login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
+
 
 /*
   Generated class for the Login page.
@@ -12,8 +15,30 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html'
 })
 export class LoginPage {
+	data : any;
+	fetchdata : any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http : Http) {
+  	this.data = {};
+  	this.data.username = "";
+  	this.data.password = "";
+  }
+
+  login(){
+  	let username = this.data.username;
+  	let password = this.data.password;
+  	let data = JSON.stringify({username, password});
+  	let link = "https://makeorder.000webhostapp.com/login.php";
+
+  	this.http.post(link, data)
+  		.map(res => res.json())
+  		.subscribe(data=>{
+  			this.fetchdata = data;
+  			console.log(this.fetchdata);
+  		}, error =>{
+  			console.log("error");
+  		});
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
