201d03fef0f90b65cf13e605baff5dad473db190
majlo23 <majo.simovic@gmail.com>
2017-04-11 13:05:45 +0200

    sending order to database

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import {HttpProvider} from '../../providers/http-provider';
+import 'rxjs/add/operator/map';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'page-page1',
@@ -10,23 +12,27 @@ import {HttpProvider} from '../../providers/http-provider';
 })
 export class Page1 {
 
-	newsData: any;
+	productData: any;
 	loading: any;
+  shoppingCart: any;
 
-  constructor(public navCtrl: NavController, private httpProvider:HttpProvider, private loadingCtrl: LoadingController ) {
+  constructor(public navCtrl: NavController, private httpProvider:HttpProvider, private loadingCtrl: LoadingController, private http: Http ) {
   this.loading = this.loadingCtrl.create({
   content: `
   <ion-spinner ></ion-spinner>`
 	});
 
   this.getdata(); 
+  this.shoppingCart={}; 
+  this.shoppingCart.count=" ";
+  this.shoppingCart.id=" ";
   }
 
   getdata(){
   this.httpProvider.getJsonData().subscribe(
     result => {
-      this.newsData=result;
-      console.log("Success : "+this.newsData);
+      this.productData=result;
+      console.log("Success : "+this.productData);
     },
     err =>{
       console.error("Error : "+err);
@@ -35,6 +41,26 @@ export class Page1 {
       console.log('getData completed');
     }
   );
+  }
+  add(itemID){
+    let id_customer = 4;
+    let id_table = 1;
+    let id = itemID;
+    let count = this.shoppingCart.count;
+    let food: { id: number, quantity: number }[] = [
+    { "id": id, "quantity": count },
+    ];
+    let shoppingCart = JSON.stringify({id_customer, id_table, food});
+    let link = "https://makeorder.000webhostapp.com/createOrder.php";
+        this.http.post(link, shoppingCart)
+      .map(res => res.json())
+      .subscribe(data=>{
+        
+      }, error =>{
+        
+      });
+    console.log("id: " + id + " count: " +count );
+ 
   }
 
 }
